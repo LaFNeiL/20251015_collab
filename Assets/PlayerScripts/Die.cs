e850e5ff13a81f10df75e3c4be4d6640ddb56637
전창현 <outgoing93@gmail.com>
2025-10-15 16:29:03 +0900

    refactor : 낙사 판정 추가

--- Assets/PlayerScripts/Die.cs
+++ Assets/PlayerScripts/Die.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -11,7 +12,7 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if (isDead) return; //이미 죽었으면 무시
 
-        if (collision.gameObject.CompareTag("Pipe")) //파이프에 닿으면 Die메서드 호출
+        if (collision.gameObject.CompareTag("Pipe") || transform.position.y < -25.0f) //파이프에 닿으면 Die메서드 호출
         {
             Die();
         }
